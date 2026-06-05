11c04d99dc87e70513dda6356bf2459aa865f30a
Elev1e1n Sure <elev1e1n@gmail.com>
2026-06-05 20:37:54 +0300

    chore: bump version to 1.2.6

--- installer.iss
+++ installer.iss
@@ -1,7 +1,7 @@
 [Setup]
 AppId={{7F8E9D2A-4B1C-5F3A-9E2D-8A7B6C5D4E3F}}
 AppName=Input VPN
-AppVersion=1.2.5
+AppVersion=1.2.6
 AppPublisher=Input VPN
 DefaultDirName={autopf}\InputVPN
 DefaultGroupName=Input VPN
--- lib/globals/app_constants.dart
+++ lib/globals/app_constants.dart
@@ -1,8 +1,8 @@
 /// Centralised application constants.
 ///
 /// Update [kAppVersion] and [kBuildNumber] here for every release.
-const String kAppVersion = '1.2.5';
-const String kBuildNumber = '11';
+const String kAppVersion = '1.2.6';
+const String kBuildNumber = '12';
 
 /// Human-readable full version string used in UI labels.
 String get kAppVersionLabel => 'v$kAppVersion';
