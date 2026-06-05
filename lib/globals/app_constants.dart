56e73bebeaffa1813cf514c3cfc000a71ecb15ba
Elev1e1n Sure <elev1e1n@gmail.com>
2026-06-05 20:55:07 +0300

    chore: bump version to 1.2.7

--- lib/globals/app_constants.dart
+++ lib/globals/app_constants.dart
@@ -1,8 +1,8 @@
 /// Centralised application constants.
 ///
 /// Update [kAppVersion] and [kBuildNumber] here for every release.
-const String kAppVersion = '1.2.6';
-const String kBuildNumber = '12';
+const String kAppVersion = '1.2.7';
+const String kBuildNumber = '13';
 
 /// Human-readable full version string used in UI labels.
 String get kAppVersionLabel => 'v$kAppVersion';
